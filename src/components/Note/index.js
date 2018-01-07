f6d5c7059023e92218c92bc93ec5dec0af1236b6
Dave Gumba <dave_gumba@Yahoo.com>
2018-01-07 05:21:39 -0500

    study mode toggle

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-const Note = ({ term, definition }) => {
+const Note = ({ term, definition, studyMode }) => {
   return (
-      <div class="note">
-        <p class="term">
+      <div className="note">
+        <p className="term">
           {term}
         </p>
 
-        <ul class="definition">
-          {definition.map((point, i) => {
-            return (
-              <li key={i} dangerouslySetInnerHTML={ {__html: point} }>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={studyMode ? "study-mode" : null}>
+          <ul class="definition">
+            {definition.map((point, i) => {
+              return (
+                <li
+                  key={i}
+                  dangerouslySetInnerHTML={ {__html: point} }
+                >
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
   );
 }
--- src/components/Notebook/index.js
+++ src/components/Notebook/index.js
@@ -4,27 +4,50 @@ import Toggle from 'react-toggle';
 import "react-toggle/style.css"
 import './styles.css';
 
-const Notebook = ({ notes }) => {
-  return (
-    <div>
+class Notebook extends Component {
 
-      <label>
-        <Toggle
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      studyMode: false,
+    }
+
+    this.toggleStudyMode = this.toggleStudyMode.bind(this);
+  }
+
+  toggleStudyMode() {
+    this.setState({ studyMode: !this.state.studyMode });
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          <Toggle
+            defaultChecked={this.state.studyMode}
+            onChange={this.toggleStudyMode}
           />
-        <span>Study Mode</span>
-      </label>
+          <span>Study Mode</span>
+        </label>
 
-      <ol className="notebook">
-        {notes.map((note, i) => {
-          return (
-            <li key={i}>
-              <Note term={note.term} definition={note.definition} />
-            </li>
-          );
-        })}
-      </ol>
-    </div>
-  );
+        <ol className="notebook">
+          {this.props.notes.map((note, i) => {
+            return (
+              <li key={i}>
+                <Note
+                  term={note.term}
+                  definition={note.definition}
+                  studyMode={this.state.studyMode} 
+                />
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
+
 }
 
 export default Notebook;
